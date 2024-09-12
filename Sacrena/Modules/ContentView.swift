31336a9173f5ed123400ac6e276756ae74cfc493
GaneshRajuGalla <ganeshrajugalla@gmail.com>
2024-09-12 20:24:54 +0530

    initial setup

---
+++ Sacrena/Modules/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sacrena
+//
+//  Created by Ganesh Raju Galla on 12/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sacrena/SacrenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SacrenaApp.swift
+//  Sacrena
+//
+//  Created by Ganesh Raju Galla on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SacrenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
